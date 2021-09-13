b9d5a0887c5d4f969bb84b6abe3f81cb48552908
Zeynep Ferah Akkurt <46840115+zefea@users.noreply.github.com>
2021-09-14 00:52:55 +0300

    added data augmentation

--- train.py
+++ train.py
@@ -57,6 +57,12 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 # create the model
 num_classes = 6
 
+data_augmentation = keras.Sequential([
+    layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+])
+
 model = Sequential([
     # data_augmentation,
     layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
